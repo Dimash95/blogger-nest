9a7e472e3eb3a53661795a6b8ed18f5778822988
Dimash <dinmukhamed.amirov@gmail.com>
2026-02-13 12:06:49 +0500

    feat: add auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,10 +20,14 @@ import { MailerModule } from '@nestjs-modules/mailer';
       transport: {
         host: 'smtp.gmail.com',
         port: 587,
+        secure: false,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
         },
+      },
+      defaults: {
+        from: `"No Reply" <${process.env.EMAIL_USER}>`,
       },
     }),
     UsersModule,
