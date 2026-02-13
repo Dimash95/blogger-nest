dfe7d72a9573137b6360ba908dae66208deef17a
Dimash <dinmukhamed.amirov@gmail.com>
2026-02-13 11:44:43 +0500

    feat: add auth

--- src/testing/testing.module.ts
+++ src/testing/testing.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TestingController } from './testing.controller';
 import { TestingService } from './testing.service';
+import { Comment, CommentSchema } from '../comments/comment.schema';
+import { Post, PostSchema } from '../posts/post.schema';
+import { User, UserSchema } from '../users/user.schema';
+import { Blog, BlogSchema } from '../blogs/blog.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Comment.name, schema: CommentSchema },
+      { name: Post.name, schema: PostSchema },
+      { name: User.name, schema: UserSchema },
+      { name: Blog.name, schema: BlogSchema },
+    ]),
+  ],
   controllers: [TestingController],
   providers: [TestingService],
 })
--- src/testing/testing.service.ts
+++ src/testing/testing.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Comment, CommentDocument } from '../comments/comment.schema';
+import { Post, PostDocument } from '../posts/post.schema';
+import { User, UserDocument } from '../users/user.schema';
+import { Blog, BlogDocument } from '../blogs/blog.schema';
 
 @Injectable()
 export class TestingService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
+    @InjectModel(Post.name) private postModel: Model<PostDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(Blog.name) private blogModel: Model<BlogDocument>,
+  ) {}
 
   async deleteAllData() {
-    await this.prisma.comment.deleteMany({});
-    await this.prisma.post.deleteMany({});
-    await this.prisma.user.deleteMany({});
-    await this.prisma.blog.deleteMany({});
-
+    await this.commentModel.deleteMany({});
+    await this.postModel.deleteMany({});
+    await this.userModel.deleteMany({});
+    await this.blogModel.deleteMany({});
     return { message: 'All data has been deleted' };
   }
 }
