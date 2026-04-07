d0053c01bff0ecc64924c8ca4dcfcf153749cf5d
Dimash <dinmukhamed.amirov@gmail.com>
2026-04-07 11:24:28 +0500

    fix: email port

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,8 +17,8 @@ import { MailerModule } from '@nestjs-modules/mailer';
     MailerModule.forRoot({
       transport: {
         host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
